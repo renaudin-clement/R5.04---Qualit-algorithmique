2ea4da90757f9a16b37e1f7362dd9a92501d581d
renaudin-clement <pro.renaudin.clement@gmail.com>
2025-09-11 11:59:49 +0200

    affichage

--- TD/td2 introduc au c/td2.1.c
+++ TD/td2 introduc au c/td2.1.c
@@ -9,7 +9,26 @@ struct maillon {
 
 typedef struct maillon * liste;
 
+int longueur(liste l){
+    if (l == NULL)
+    {
+        return 0;
+    } else {
+        return longueur(l->suivant) + 1;
+    }
+    
+}
 
+void affiche(liste l){
+    printf("[");
+    struct maillon * courant = l;
+    while ((courant !=NULL)){
+        printf("%d ,", courant->val);
+        courant = courant->suivant;
+    }
+    printf("]");
+    
+}
 
 int main(){
     liste liste_vide = NULL;
@@ -17,9 +36,15 @@ int main(){
     struct maillon m3 = {3,NULL};
     struct maillon m7 = {7,NULL};
     m3.suivant = &m7;
-    
+
     liste liste_un_elt = &depart;
     liste liste_deux_elt = &m3;
+
+    printf("la longueur de la liste_deux-elt est %d\n",longueur(liste_deux_elt));
+    affiche(liste_deux_elt);
+    printf("\n");
+    affiche(liste_un_elt);
+    printf("\n");
     
     
 
