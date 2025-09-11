56716667d91e05c6d1b4b84d25c4bb4e3c206453
renaudin-clement <pro.renaudin.clement@gmail.com>
2025-09-11 12:28:37 +0200

    toujours erreur mais cherche

--- TD/td2 introduc au c/td2.1.c
+++ TD/td2 introduc au c/td2.1.c
@@ -27,17 +27,24 @@ void affiche(liste l){
         courant = courant->suivant;
     }
     printf("]");
+}
+
+liste insere_en_tete(liste l ,int v){
+    struct maillon tete ={v, l};
+    l = &tete;
+    return l;
     
 }
 
 int main(){
     liste liste_vide = NULL;
     struct maillon depart = {0,NULL};
+    liste liste_un_elt = &depart;
     struct maillon m3 = {3,NULL};
     struct maillon m7 = {7,NULL};
     m3.suivant = &m7;
 
-    liste liste_un_elt = &depart;
+   
     liste liste_deux_elt = &m3;
 
     printf("la longueur de la liste_deux-elt est %d\n",longueur(liste_deux_elt));
@@ -45,9 +52,14 @@ int main(){
     printf("\n");
     affiche(liste_un_elt);
     printf("\n");
+
+    liste liste_trois_elt = insere_en_tete(liste_deux_elt,-43);
+    affiche(liste_trois_elt);
+    printf("\n");
     
     
 
     return 1;
 }
 
+//voir utilisation malloc
