1c1ef918c650312a2caf654c2a1409fa215517f7
renaudin-clement <pro.renaudin.clement@gmail.com>
2025-09-11 11:28:59 +0200

    je sais pas bizarre

--- TD/td2 introduc au c/td2.c
+++ TD/td2 introduc au c/td2.c
@@ -1,5 +1,22 @@
 #include <stdio.h>
 
+
+int main(){
+
+    int x = 3;
+    int *px = &x;
+    *px = *px+1 ;
+    
+    printf("%d \n",x);
+    printf("j'affiche le pointeur en faisant une tab\t %p \t son address memoire \n",px);
+    *px += 1 ;
+    printf("j'affiche le pointeur en faisant une tab\t %p \t son address memoire \n",px + 1);
+
+
+    return 1;
+}
+
+
 struct maillon {
     int val;
     struct maillon * suivant;
@@ -7,5 +24,10 @@ struct maillon {
 
 struct maillon depart = {0,NULL};
 
-int x = 3;
-int *px = &x;
+
+/*
+    int x = 3;
+    int *px = &x
+    *px = px + 1
+    printf("%d\n",x); // stdout: "-1510730656" ?!
+*/
